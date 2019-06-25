2f85ba0e9c757f21fed30e3796feda4f71b08333
nataliukecoffee <nataliuke.coffee@gmail.com>
2019-06-25 22:43:49 +0300

    Piesiame

---
+++ PiesiameIsZvaigzduciu/ConsoleApp71/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= 4 - i; j++)
+                {
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
