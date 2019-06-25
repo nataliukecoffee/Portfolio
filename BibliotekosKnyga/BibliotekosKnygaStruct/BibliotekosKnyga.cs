f2e8d0508bbe90d4d1f79854d4ef715d0c95d2d0
nataliukecoffee <nataliuke.coffee@gmail.com>
2019-06-25 22:16:31 +0300

    BibliotekosKnyga

---
+++ BibliotekosKnyga/BibliotekosKnygaStruct/BibliotekosKnyga.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BibliotekosKnygaStruct
+{
+    public class BibliotekosKnyga
+    {
+
+        public int Id { get; private set; }
+        public string Pavadinimas { get; private set; }
+        public int PerKiekDienuGrazinti { get; private set; }
+        public string SkaitytojoVardas { get; set; }
+        public DateTime _paemimoData { get; set; }
+
+
+        public BibliotekosKnyga(int id, string pavadinimas,
+            int perKiekDienuGrazinti,
+            string skaitytojoVardas,
+            DateTime paemimoData)
+        {
+            Id = id;
+            Pavadinimas = pavadinimas;
+            PerKiekDienuGrazinti = perKiekDienuGrazinti;
+            SkaitytojoVardas = skaitytojoVardas;
+            _paemimoData = paemimoData;
+        }
+
+        public bool PavelavoGrazintiKnyga()
+        {
+            
+            var pavelavoGrazinti = true;
+            var perKiekDienuGrazino = (DateTime.Now.Day - _paemimoData.Day); 
+
+            if (perKiekDienuGrazino < perKiekDienuGrazino)
+            {
+                pavelavoGrazinti = true;
+            }
+            else
+            {
+                pavelavoGrazinti = false;
+            }
+            return pavelavoGrazinti;
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+    }
+}
---
+++ BibliotekosKnyga/BibliotekosKnyga_KlasesNr2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotekosKnygaStruct;
+
+namespace BibliotekosKnyga_KlasesNr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BibliotekosKnyga knyga1 = new BibliotekosKnyga(101, "Gera Knyga", 14, "Natalija", new DateTime(2019,06,01));
+            Console.WriteLine("{0}. {1}, {2}, {3}, {4}",  knyga1.Id, 
+                knyga1.Pavadinimas, knyga1.PerKiekDienuGrazinti, 
+                knyga1.SkaitytojoVardas, knyga1.PerKiekDienuGrazinti);
+  
+            Console.ReadLine();
+        }
+
+        
+    }
+}
