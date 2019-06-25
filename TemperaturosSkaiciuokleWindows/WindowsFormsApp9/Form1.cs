5eef6d58c6a85b508254d7a11953987a0bfdeeec
nataliukecoffee <nataliuke.coffee@gmail.com>
2019-06-25 22:24:33 +0300

    TemperaturosSkaiciuokle

---
+++ TemperaturosSkaiciuokleWindows/WindowsFormsApp9/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double laipsniai;
+            double kelvin;
+            double faringeit;
+            double reomiur;
+
+            laipsniai = Convert.ToDouble(textBox1.Text);
+
+            kelvin = laipsniai + 273.16;
+            faringeit = laipsniai * (9 / 5) + 32;
+            reomiur = 0.8 * laipsniai;
+
+            textBox2.Text = kelvin.ToString();
+            textBox3.Text = faringeit.ToString();
+            textBox4.Text = reomiur.ToString();
+            
+        }
+    }
+}
