1da659569d6a340bb8052661933980be964a3b89
nataliukecoffee <nataliuke.coffee@gmail.com>
2019-06-25 22:03:05 +0300

    ZmogusStrukturos

---
+++ ZmogusStrukturos/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        struct Zmogus
+        {
+            public string Vardas;
+            public string Pavarde;
+        };
+        static void Main(string[] args)
+        {
+            Zmogus zmogus1;
+            zmogus1.Vardas = "Natalija";
+            zmogus1.Pavarde = "Griadovskaja";
+
+            Console.WriteLine(zmogus1.Vardas + " " + zmogus1.Pavarde);
+            Console.ReadLine();
+        }
+    }
+}
